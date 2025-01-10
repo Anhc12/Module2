0aab131784cdf24ab794c0ed2b088cc8f03bfec6
Hoang Anh <anhnh265@gmail.com>
2025-01-11 00:54:25 +0700

    thuc hanh static

---
+++ bai5_MonifierAndStatic/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        Student.change();
+
+        Student s1 = new Student(1,"hoang");
+        Student s2 = new Student(2,"khoang");
+        Student s3 = new Student(3,"toang");
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
